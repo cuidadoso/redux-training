699fa1f77cd4822f2696270c0ba639f55c14e11f
cuidadoso <alexander_s@front.ru>
2016-11-20 19:28:55 +0200

    Add asynchronous functionality for user. Add VK API functionality. Add style.

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,15 +4,17 @@ import { connect } from 'react-redux';
 import User from '../components/User';
 import Page from '../components/Page';
 import * as pageActions from '../actions/PageActions';
+import * as userActions from '../actions/UserActions';
 
 class App extends Component {
   render() {
       const { user, page } = this.props;
       const { getPhotos } = this.props.pageActions;
+      const { handleLogin } = this.props.userActions;
 
       return <div>
-          <User name={ user.name }/>
           <Page year={page.year} photos={page.photos} fetching={page.fetching} getPhotos={getPhotos}/>
+          <User name={user.name} handleLogin={handleLogin} error={user.error} />
       </div>
   }
 }
@@ -26,7 +28,8 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        pageActions: bindActionCreators(pageActions, dispatch)
+        pageActions: bindActionCreators(pageActions, dispatch),
+        userActions: bindActionCreators(userActions, dispatch)
     }
 }
 
