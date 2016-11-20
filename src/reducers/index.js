69cc2d4c6bf567e13a5b87b94a468ce4f77a996c
cuidadoso <alexander_s@front.ru>
2016-11-20 16:32:27 +0200

    Add Redux initial store and reducer - rootReducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alejandro on 20.11.2016.
+ */
+const initialState = {
+    name: 'Василий',
+    surname: 'Реактов',
+    age: 27
+};
+
+export default function userstate(state = initialState) {
+    return state;
+}
