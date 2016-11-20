a6323b7ff54fdee415c0c297cd6b3b28d53f15bb
cuidadoso <alexander_s@front.ru>
2016-11-20 18:48:41 +0200

    Add asynchronous functionality.

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -1,11 +1,21 @@
 /**
  * Created by Alejandro on 20.11.2016.
  */
-import { SET_YEAR } from '../constants/Page';
+import { GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS } from '../constants/Page';
 
-export function setYear(year) {
-    return {
-        type: SET_YEAR,
-        payload: year
+export function getPhotos(year) {
+
+    return (dispatch) => {
+        dispatch({
+            type: GET_PHOTOS_REQUEST,
+            payload: year
+        })
+
+        setTimeout(() => {
+            dispatch({
+                type: GET_PHOTOS_SUCCESS,
+                payload: [1,2,3,4,5]
+            })
+        }, 1000)
     }
 }
--- src/constants/Page.js
+++ src/constants/Page.js
@@ -1,4 +1,5 @@
 /**
  * Created by Alejandro on 20.11.2016.
  */
-export const SET_YEAR = 'SET_YEAR';
+export const GET_PHOTOS_REQUEST = 'GET_PHOTOS_REQUEST';
+export const GET_PHOTOS_SUCCESS = 'GET_PHOTOS_SUCCESS';
